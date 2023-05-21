23918c08affe1ae5ba628ca1a0bae681c463adc8
NormalMood <littlebigdiamond@gmail.com>
2023-05-21 21:07:35 +0300

    feature: add lombok

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "usr")
+@Getter
+@NoArgsConstructor
 public class User {
 	
 	@Id
@@ -13,27 +17,5 @@ public class User {
 	private Long id;
 	
 	private String name;
-	
-	public User() {
-		
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
 
 }
